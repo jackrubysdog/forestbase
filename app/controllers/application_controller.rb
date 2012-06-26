9e3ae848cc42e6e479cfe3184cfb9a40b2fe647a
Peter Attaway <jackrubysdog@o2.co.uk>
2012-06-26 16:11:08 +0100

    DNS stuff

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :ensure_domain, :authorize
   
   require './requires/random_image'
   require './requires/season_record'
   require './requires/player_season_record'
   require './requires/player_record'
-  require './requires/opponent_record'   
+  require './requires/opponent_record'  
+  
+  def ensure_domain 
+    if request.env['HTTP_HOST'] != APP_ADDRESS && ENV["RAILS_ENV"] != 'development' 
+      redirect_to("http://" + AP_ADDRESS) 
+    end 
+  end 
 
 end
---
+++ config/initializers/constants.rb
@@ -0,0 +1,1 @@
+APP_ADDRESS = 'www.forestbase.co.uk'
