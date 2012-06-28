ddf740a236fdc6e0b1e4a8fd6add92fb96598fb8
Peter Attaway <jackrubysdog@o2.co.uk>
2012-06-28 20:17:08 +0100

    domain mask error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  before_filter :ensure_domain
+  # before_filter :ensure_domain
   
   require './requires/random_image'
   require './requires/season_record'
@@ -8,10 +8,13 @@ class ApplicationController < ActionController::Base
   require './requires/player_record'
   require './requires/opponent_record'  
   
-  def ensure_domain 
-    if request.env['HTTP_HOST'] != APP_ADDRESS && ENV["RAILS_ENV"] != 'development' 
-      redirect_to("http://" + AP_ADDRESS) 
-    end 
-  end 
+  #
+  # RESTORE FOR DOMAIN MASK
+  #
+  # def ensure_domain 
+  #  if request.env['HTTP_HOST'] != APP_ADDRESS && ENV["RAILS_ENV"] != 'development' 
+  #    redirect_to("http://" + AP_ADDRESS) 
+  #  end 
+  # end 
 
 end
